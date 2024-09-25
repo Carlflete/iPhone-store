9900ef2eb934b2dee94bfa1394088ba8d8bd8192
Maykhel Valero <valerom140@gmail.com>
2024-09-25 17:04:22 -0400

    agrega esto para el proyecto

---
+++ main.js
@@ -0,0 +1,11 @@
+let usuario = {
+    name: "carlos",
+    lastname: "flete",
+    age:17,
+    hair: "#000",
+    address: {
+    city: "Carabobo",
+    country: "Venezuela",
+    street: "main street"
+ }
+}
